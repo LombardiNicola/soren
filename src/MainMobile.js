1c1c27d7391c3bec1789629b45592a5d155d072d
Nicola Lombardi <dev.nicolalombardi@gmail.com>
2018-09-25 18:42:13 +0200

    Updates

--- src/MainMobile.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-export default class helloWorld extends Component {
-    state = {  }
-    render() { 
-        return ( <div>Hello World</div> );
-    }
-}
